b615bb2ba96be5780b6ce085f748f90b4bba886d
Francis Bergonia <francisb.itcareers@gmail.com>
2019-11-17 22:09:32 +0800

    initial commit

---
+++ Huli Pizza/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Huli Pizza
+//
+//  Created by Francis Jemuel Bergonia on 11/17/19.
+//  Copyright © 2019 Francis Jemuel Bergonia. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Text("Huli Pizza Company")
+            Image("Surf Board")
+            Text("Order Pizza")
+            Spacer()
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
